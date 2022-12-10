07ed08089c9c7100878d9fa74523e4c52447179b
Stanislav Smirnov <stanislav@laont.me>
2022-12-10 23:53:09 +0300

    Tweak some configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard-with-typescript'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    linkComponents: [
+      { name: 'Link', linkAttribute: 'to' }
+    ]
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: [
+      './tsconfig.json',
+      './tsconfig.node.json'
+    ]
+  }
+}
